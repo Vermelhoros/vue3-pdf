594edc840cc5a71a99f1306c5eb709dadd443007
Denis Rosso <dnsrosso8@gmail.com>
2023-10-20 18:59:42 -0400

    Adding toolbar

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,31 @@
+import AdsibIcon from './Adsib.svg';
+import AgeticIcon from './AgeticIcon.svg';
+import Barra2Icon from './Barra2.svg';
+import BarraIcon from './BarraIcon.svg';
+import DownIcon from './DownIcon.svg';
+import DownloadIcon from './DownloadIcon.svg';
+import FileIcon from './FileIcon.svg';
+import FileSimpleIcon from './FileSimpleIcon.svg';
+import FullScreenIcon from './FullscreenIcon.svg';
+import PrintIcon from './PrintIcon.svg';
+import SearchIcon from './SearchIcon.svg';
+import UpIcon from './UpIcon.svg';
+import ZoomInIcon from './ZoomInIcon.svg';
+import ZoomOutIcon from './ZoomOutIcon.svg';
+
+export {
+  AdsibIcon,
+  AgeticIcon,
+  Barra2Icon,
+  BarraIcon,
+  DownIcon,
+  DownloadIcon,
+  FileIcon,
+  FileSimpleIcon,
+  FullScreenIcon,
+  PrintIcon,
+  SearchIcon,
+  UpIcon,
+  ZoomInIcon,
+  ZoomOutIcon
+};
